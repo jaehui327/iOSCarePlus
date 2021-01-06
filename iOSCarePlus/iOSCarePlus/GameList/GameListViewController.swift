4a2de2452e4afe24b6a7d0e487ac22b6b79eb38b
jaehui327 <jaeheekim970327@gmail.com>
2021-01-06 12:28:15 +0900

    :wrench: add GameListViewController, second tab

---
+++ iOSCarePlus/iOSCarePlus/GameList/GameListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GameListViewController.swift
+//  iOSCarePlus
+//
+//  Created by JHKim on 2021/01/06.
+//
+
+import UIKit
+
+class GameListViewController: UIViewController {
+    // MARK: - IBOutlet
+    @IBOutlet private weak var tableView: UITableView!
+    
+    // MARK: - properties
+    
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
