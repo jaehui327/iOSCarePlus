eff10347dec743ad3597a7f6d11a621b981a674b
jaehui327 <jaeheekim970327@gmail.com>
2021-01-20 20:09:35 +0900

    :sparkles: 3주차 과제 - response model에 screenshots 추가

--- iOSCarePlus/iOSCarePlus/GameList/Model/NewGameResponse.swift
+++ iOSCarePlus/iOSCarePlus/GameList/Model/NewGameResponse.swift
@@ -7,13 +7,23 @@
 
 import Foundation
 
+struct Images: Decodable {
+    let url: String
+}
+
+struct ScreenShots: Decodable {
+    let images: [Images]
+}
+
 struct NewGameContent: Decodable {
     let formalName: String
     let heroBannerURL: String
+    let screenshots: [ScreenShots]
     
     enum CodingKeys: String, CodingKey {
         case formalName = "formal_name"
         case heroBannerURL = "hero_banner_url"
+        case screenshots
     }
 }
 
