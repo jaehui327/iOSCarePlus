c63a8e7699f8b2235832682f1a164b38a7f341a0
jaehui327 <jaeheekim970327@gmail.com>
2021-01-27 20:32:33 +0900

    🎉 add Week7 project - UIScrollView

---
+++ Week7/Week7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week7
+//
+//  Created by dev on 2021/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
