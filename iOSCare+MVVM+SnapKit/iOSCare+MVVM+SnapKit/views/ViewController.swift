504bd8366fae1604caf8d632eae6e9f37115f558
jaehui327 <jaeheekim970327@gmail.com>
2021-02-15 21:00:37 +0900

    🎉 add iOSCare+MVVM+Snapkit project

---
+++ iOSCare+MVVM+SnapKit/iOSCare+MVVM+SnapKit/views/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iOSCare+MVVM+SnapKit
+//
+//  Created by dev on 2021/02/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
