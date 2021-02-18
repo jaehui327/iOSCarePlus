70afa04cec32b388bc34e14460f7b299d668f155
zhilogs <jaeheekim970327@gmail.com>
2021-02-18 22:00:34 +0900

    📦 add pods

--- iOSCare+MVVM+SnapKit/iOSCare+MVVM+SnapKit/views/ViewController.swift
+++ iOSCare+MVVM+SnapKit/iOSCare+MVVM+SnapKit/views/ViewController.swift
@@ -12,6 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.backgroundColor = UIColor(named: "dusk")
     }
 }
